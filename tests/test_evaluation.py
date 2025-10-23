747ef97bc25bcff140c2f766c5e5b3a2ea5d0ec8
PDalm214 <torrespalejandro428@gmail.com>
2025-10-23 20:25:26 +0000

    entrega

--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -29,6 +29,7 @@ BOLD = "\033[1m"
 SEPARATOR = f"{BOLD}{'='*50}{RESET}"
 
 SOURCE_URL = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%202/data/FuelConsumptionCo2.csv"
+CHURN_URL = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/ChurnData.csv"                                                          
 OUTPUT_DIR = "test_outputs"
 FEATURE_1 = "ENGINESIZE"
 FEATURE_2 = "FUELCONSUMPTION_COMB"
@@ -171,14 +172,14 @@ class TestEvaluationOne(unittest.TestCase):
         self.assertEqual(status, os.EX_OK, "main() no terminó con EX_OK")
 
         # Verificar que intro.csv_registers funciona
-        total, df = ipd.csv_registers(main.FILE)
+        total, df = ipd.csv_registers(main.CSV_FILE)
         self.assertIsNotNone(total, "csv_registers devolvió total = None")
         self.assertIsInstance(df, pd.DataFrame, "csv_registers no devolvió un DataFrame válido")
         self.assertFalse(df.empty, "csv_registers devolvió un DataFrame vacío")
 
         # Verificar existencia de archivos de salida
-        output_csv = os.path.join(os.path.dirname(main.FILE), "..", "outputs", "aprobados.csv")
-        output_plot = os.path.join(os.path.dirname(main.FILE), "..", "analisis.png")
+        output_csv = os.path.join(os.path.dirname(main.CSV_FILE), "..", "outputs", "aprobados.csv")
+        output_plot = os.path.join(os.path.dirname(main.CSV_FILE), "..", "outputs", "analisis.png")
         self.assertTrue(os.path.exists(output_csv), "No se encontró 'aprobados.csv'")
         self.assertTrue(os.path.exists(output_plot), "No se encontró 'analisis.png'")
 
